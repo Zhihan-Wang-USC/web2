609b0a3eef4e6324c3073dd08917509b5feaaaf5
Zhihan-Wang-USC <zhihanwa@usc.edu>
2020-06-18 11:01:43 +0800

    v0

---
+++ react_tic-tac-toe/precache-manifest.7fdc0da9d4f83330881b4b9f1a41e03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210cd2e554884eecc80988d574eb9246",
+    "url": "/react_tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f5acdf8cc9ddac604598",
+    "url": "/react_tic-tac-toe/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/react_tic-tac-toe/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tic-tac-toe/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5acdf8cc9ddac604598",
+    "url": "/react_tic-tac-toe/static/js/main.9ac2b059.chunk.js"
+  },
+  {
+    "revision": "2bd449c5e58403dcb52d",
+    "url": "/react_tic-tac-toe/static/js/runtime-main.86cce9bb.js"
+  }
+]);
